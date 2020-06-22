f72d45a6337ebf287ae7768cb34033a1c3c95fe3
plugker <67256074+plugker@users.noreply.github.com>
2020-06-23 00:19:57 +0800

    Update path.dart

--- plugker/setting/path.dart
+++ plugker/setting/path.dart
@@ -1,18 +1,10 @@
-import 'package:navigation/App/Button/ButtonView.dart';
-import 'package:navigation/App/Card/cardView.dart';
-import 'package:navigation/App/Installation/installationView.dart';
-import 'package:navigation/App/Introduction/IntroductionView.dart';
-import 'package:navigation/App/Routing/routingView.dart';
-
 import '../engine/router/router.dart';
 import 'package:flutter/material.dart';
 
 List<dynamic> routes(RouteSettings settings) {
   return [
-    Router.path('introduction', IntroductionView()),
-    Router.path('installation', InstallationView()),
-    Router.path('routing', RoutingView()),
-    Router.path('button', ButtonView()),
-    Router.path('card', CardView()),
-  ];
+    //Register path here
+    //Example code
+    //Router.path('name path', YourClass()),
+    ];
 }
